1a93875ae7b43d2f50d661abe28b926a40f31b87
phuphuc <phuphuc2k1@gmail.com>
2024-01-18 08:08:02 +0700

    Add Contact and Detail files crud

--- Models/Contact/Contact.cs
+++ Models/Contact/Contact.cs
@@ -12,17 +12,23 @@ namespace App.Models.Contacts {
 
         [Column(TypeName = "nvarchar")]
         [StringLength(50)]
-        [Required]
+        [Required( ErrorMessage = "Phải nhập {0}")]
+        [Display(Name = "Họ Và Tên")]
         public string FullName {set;get;}  = "";
         
-        [Required]
+        [Required(ErrorMessage = "Phải nhập {0}")]
         [StringLength(100)]
+        [EmailAddress(ErrorMessage = "Phải là địa chỉ {0}")]
         public string Email {set;get;}  ="";
 
         public DateTime DateSent {set;get;}
+
+        [Display(Name = "Tin nhắn")]
         public string? Message {set;get;} 
 
         [StringLength(50)]
+        [Display(Name = "Số điện thoại")]
+        [Required(ErrorMessage = "Cần phải nhập {0}")]
         public string Phone {set;get;} = "";
     }
 }
